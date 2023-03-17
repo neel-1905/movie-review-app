f64e1b81fcadb432b3386d3ce825ff8283fd4266
neel-1905 <neelnarwadkar@gmail.com>
2023-03-17 13:11:17 +0530

    origin update

--- movie-review-app-backend/index.js
+++ movie-review-app-backend/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://moviereview-app-frontend.netlify.app",
+    origin: "https://moviereview-app-frontend.netlify.app/",
     credentials: true,
   })
 );
