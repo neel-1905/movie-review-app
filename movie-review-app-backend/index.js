7cd4eb7df3590a19fcac5c170696596d2f262cc6
neel-1905 <neelnarwadkar@gmail.com>
2023-03-18 10:21:42 +0530

    set header

--- movie-review-app-backend/index.js
+++ movie-review-app-backend/index.js
@@ -11,12 +11,17 @@ const adminRoutes = require("./routes/admin");
 const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(express.urlencoded({ limit: "15mb" }));
 app.use(
   cors({
     // origin: "https://moviereview-app-frontend.netlify.app/",
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(cookieParser());
 
 // ROUTES
